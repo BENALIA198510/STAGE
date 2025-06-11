12ae0de82af1493426f428e1717712e22e5fc7b9
BENALIA198510 <BENALIA1985@GMAIL.COM>
2025-06-11 11:51:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/STAGE/', // 👈 هذا السطر مهم ليعمل الموقع على GitHub Pages
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
+
